01b9d9ec0431235e6ed5d705f58e26d0d0c83b35
Sergey Korobin <cerkill@mail.ru>
2017-09-14 14:32:35 +0300

    Initial Commit

---
+++ Tinkoff converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinkoff converter
+//
+//  Created by Sergey Korobin on 14.09.17.
+//  Copyright © 2017 Korob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
