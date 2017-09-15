9354e47a8784aededc13ce9e9c7dea73ce1d3cd8
Sergey Korobin <cerkill@mail.ru>
2017-09-16 00:33:42 +0300

    calculator changes + minor fixes

--- Tinkoff converter/ViewController.swift
+++ Tinkoff converter/ViewController.swift
@@ -8,29 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate{
     
+    // Alerts
     let internetFailAlert = UIAlertController(title: "Обновление данных", message: "Отсутствует подключение к сети.", preferredStyle: UIAlertControllerStyle.alert)
     let dataErrorAlert = UIAlertController(title: "Ошибка", message: "Данных валюты не найдено!", preferredStyle: UIAlertControllerStyle.alert)
     
-    
+    // Outlets
     @IBOutlet weak var CurrencyLabel: UILabel!
 
     @IBOutlet weak var DateLabel: UILabel!
+    
+    @IBOutlet weak var WelcomeLabel: UILabel!
     @IBOutlet weak var LoadInd: UIActivityIndicatorView!
     
+    @IBOutlet weak var DataReadyLabel: UILabel!
     @IBOutlet weak var CurrencyList_from: UIPickerView!
     
     @IBOutlet weak var CurrencyList_after: UIPickerView!
     
+    @IBOutlet weak var NumberValue: UITextField!
+    @IBOutlet weak var ValueTo: UITextField!
     @IBAction func RetryButton(_ sender: UIButton) {
         pre_request()
         if flag{
-            self.CurrencyLabel.text = "Данные обновлены!"
+             DispatchQueue.main.async {
+                self.NumberValue.text = ""
+                self.ValueTo.text = ""
+            }
         }
+    }
+    @IBAction func ChangeRates(_ sender: Any) {
+        var rightValue = self.CurrencyList_after.selectedRow(inComponent: 0)
+        var leftValue = self.CurrencyList_from.selectedRow(inComponent: 0)
+        self.CurrencyList_from.selectRow(rightValue, inComponent: 0, animated: true)
+        self.CurrencyList_after.selectRow(leftValue, inComponent: 0, animated: true)
+        var textFieldLeftData = self.NumberValue.text
+        var textFieldRightData = self.ValueTo.text
+        var clone = textFieldLeftData
+        self.NumberValue.text = textFieldRightData
+        self.ValueTo.text = clone
+        updateCurrentCurrency()
     }
     var currencies = [String]()
     var flag: Bool = false
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -64,16 +94,27 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.CurrencyLabel.text = nil
         self.CurrencyList_from.dataSource = self
         self.CurrencyList_after.dataSource = self
         self.CurrencyList_from.delegate = self
         self.CurrencyList_after.delegate = self
+        self.NumberValue.delegate = self
+        self.ValueTo.delegate = self
         self.LoadInd.hidesWhenStopped = true
+        self.NumberValue.textAlignment = .center
+        self.NumberValue.textColor = UIColor.lightGray
+        self.NumberValue.font = UIFont(name: "Futura-Medium", size: 18)
+        self.NumberValue.text = ""
+        self.NumberValue.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+        self.ValueTo.textAlignment = .center
+        self.ValueTo.textColor = UIColor.lightGray
+        self.ValueTo.font = UIFont(name: "Futura-Medium", size: 18)
+        self.ValueTo.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         self.internetFailAlert.addAction(UIAlertAction(title: "Ок", style: UIAlertActionStyle.default, handler: nil))
         self.dataErrorAlert.addAction(UIAlertAction(title: "Ок", style: UIAlertActionStyle.default, handler: nil))
         pre_request()
-        
+
         
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -84,7 +125,35 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldDidChange(_ textField: UITextField) {
+        if self.CurrencyLabel.text != "" {
+            var currency_val = Double(self.CurrencyLabel.text!)
+            var number_l = Double(self.NumberValue.text!)
+            var number_r = Double(self.ValueTo.text!)
+            if textField == NumberValue && textField.text != ""{
+                DispatchQueue.main.async {
+                    self.ValueTo.text = String(currency_val! * number_l!)
+                }
+            } else if textField == ValueTo && textField.text != ""
+            {
+                DispatchQueue.main.async {
+                    self.NumberValue.text = String(number_r! / currency_val!)
+                }
+            }
+        }
+    }
     
+    func updateRightTextField() {
+            var currency_val_cur = Double(self.CurrencyLabel.text!)
+            var number_l_cur = Double(self.NumberValue.text!)
+            var number_r_cur = Double(self.ValueTo.text!)
+            if number_l_cur != nil && currency_val_cur != nil{
+                DispatchQueue.main.async(execute: {
+                self.ValueTo.text = String(currency_val_cur! * number_l_cur!)
+                })
+            }
+    }
+
     func pre_request() {
         LoadInd.startAnimating()
         let url = URL(string: "https://api.fixer.io/latest")
@@ -112,7 +181,19 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                         if let date = json_cont["date"] as? String
                         {
                             DispatchQueue.main.async {
-                            self.DateLabel.text = date
+                                self.DateLabel.text = date
+                                self.DataReadyLabel.alpha = 0
+                                self.DataReadyLabel.text = "Данные обновлены!"
+                                self.DataReadyLabel.isHidden = false
+                                UIView.animate(withDuration: 2.5) {
+                                    self.DataReadyLabel.alpha = 1
+                                }
+                                UIView.animate(withDuration: 2.5, animations: {
+                                    self.DataReadyLabel.alpha = 0
+                                }) { (finished) in
+                                    self.DataReadyLabel.isHidden = finished
+                                }
+                                
                             }
                         }
                         self.flag = true
@@ -133,8 +214,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                 self.LoadInd.stopAnimating()
                 self.CurrencyList_after.reloadAllComponents()
                 self.CurrencyList_from.reloadAllComponents()
-                self.CurrencyList_from.selectRow(self.currencies.count/2, inComponent: 0, animated: true)
-                self.CurrencyList_after.selectRow(self.currencies.count/2 + 1, inComponent: 0, animated: true)
+                self.CurrencyList_from.selectRow(30, inComponent: 0, animated: true)
+                self.CurrencyList_after.selectRow(25, inComponent: 0, animated: true)
+                // USD to RUB popular request
+                self.WelcomeLabel.alpha = 0
+                self.WelcomeLabel.text = "Выберите Валюту"
+                self.WelcomeLabel.isHidden = false
+                UIView.animate(withDuration: 1) {
+                    self.WelcomeLabel.alpha = 1
+                }
+                self.updateCurrentCurrency()
                 
             }
             
@@ -153,16 +242,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
     }
     
-//    func baseExept() -> [String] {
-//        var cur_mod = currencies
-//        if cur_mod.count != 0{
-//            cur_mod.remove(at: CurrencyList_from.selectedRow(inComponent: 0))
-//            return cur_mod
-//        }
-//        else{
-//            return currencies
-//        }
-//    }
     
     func parseCur(data: Data?, toCurrency: String) -> String{
         var str = ""
@@ -172,22 +251,21 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             if let parsed_json = json{
                 if let cur_rates = parsed_json?["rates"] as? Dictionary<String, Double>{
                     if let elem = cur_rates[toCurrency] {
-                        str = String(format:"%.2f", elem)
+                        str = String(format:"%.3f", elem)
                     }
                     else
                     {
                         str = ""
-                        // ...
                     }
                 }
                 else
                 {
-                    str = "Can't find _rates_ in json"
+                   print("Can't find _rates_ in json")
                 }
             }
             else
             {
-                str = "JSON can't be parsed. No data available!"
+                print("JSON can't be parsed. No data available!")
                 self.present(self.dataErrorAlert, animated: true, completion: nil)
             }
         }
@@ -196,10 +274,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     func retrieveCurrency(baseCurrency: String, toCurrency: String, completion: @escaping(String) -> Void) {
         self.requestCurrency(baseCurrency: baseCurrency) { [weak self] (data, error) in
-        var string = "No currency retrieved"
+        var string = ""
         
             if let currentError = error{
-                string = currentError.localizedDescription
+                print(currentError.localizedDescription)
+                self?.present((self?.internetFailAlert)!, animated: true, completion: nil)
             }
             else{
                 string = (self?.parseCur(data: data, toCurrency: toCurrency))!
@@ -220,12 +299,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             self.retrieveCurrency(baseCurrency: baseCurrency, toCurrency: toCurrency) { [weak self] (val) in
                 DispatchQueue.main.async(execute: {
                     self?.CurrencyLabel.text = val
+                    self?.updateRightTextField()
                     self?.LoadInd.stopAnimating()
+                    
                 })
             }
         }
+
     }
-    
 
 
 }
